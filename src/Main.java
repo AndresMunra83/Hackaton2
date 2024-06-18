a6cb822eb5bb85108047cd7b596b11f4bc957d57
AndresMunra83 <edwars.munar@genstudents.org>
2024-06-18 12:23:41 -0500

    se removio el id

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
     Agenda agenda = new Agenda();
 
     //Definir objetos contactos
-    Contacto contacto1 = new Contacto(1, "Alisson", "1111111111");
-    Contacto contacto2 = new Contacto(2,"karla","222222222");
-    Contacto contacto3 = new Contacto(3,"edwars","33333333");
+    Contacto contacto1 = new Contacto("Alisson", "1111111111");
+    Contacto contacto2 = new Contacto("karla","222222222");
+    Contacto contacto3 = new Contacto("edwars","33333333");
 
     agenda.agregarContacto(contacto1);
   }
