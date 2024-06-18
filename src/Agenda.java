eb3ded6b8b513075da3b88ee5d3b82d34f416aac
AndresMunra83 <edwars.munar@genstudents.org>
2024-06-18 12:14:51 -0500

    se creo funcionalidad añadir contactos

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Agenda {
+
+    private List<Contacto> contactos;
+
+    // constructor para el array list
+
+    public Agenda (){
+        this.contactos = new ArrayList<>();
+    }
+    // se crea metodo añadir contacto
+    public void agregarContacto(int id, String nombre, String numero){
+        Contacto nuevoContacto = new Contacto( id,nombre,numero);
+        contactos.add(nuevoContacto);
+        System.out.println("El contacto ha sido creado exitosamente: " + nuevoContacto);
+    }
+
+    public void agregarContacto(Contacto contacto1) {
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
-    
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      System.out.println("i = " + i);
-    }
+    Scanner scanner = new Scanner(System.in);
+    Agenda agenda = new Agenda();
+
+    //Definir objetos contactos
+    Contacto contacto1 = new Contacto(1, "Alisson", "1111111111");
+    Contacto contacto2 = new Contacto(2,"karla","222222222");
+    Contacto contacto3 = new Contacto(3,"edwars","33333333");
+
+    agenda.agregarContacto(contacto1);
   }
 }
